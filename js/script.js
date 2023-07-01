242896877ad4e9885c9d0aeb0a9c474675b3128d
Азат Файзрахманов <azat_fayzrahmanov@mail.ru>
2023-07-01 15:57:38 +0300

    add burger and textarea autoheight

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// * faq block
 const faq_item = document.querySelectorAll('.item-faq');
 const toggleHeightBlock = (block) => {
 	if (block.style.height === '0px') {
@@ -17,7 +18,6 @@ const toggleHeightBlock = (block) => {
 		{ once: true }
 	);
 };
-
 faq_item.forEach((el) => {
 	const answer_block = el.querySelector('.item-faq__answer');
 	toggleHeightBlock(answer_block);
@@ -28,48 +28,64 @@ faq_item.forEach((el) => {
 	});
 });
 
+// * phone number input
 const telInput = document.querySelector('.tel-input');
 telInput &&
 	telInput.addEventListener('input', (e) => {
 		e.target.value = e.target.value.replace(/[^0-9.]/g, '');
 	});
 
+// * burger icon
 const burger = document.querySelector('.burger');
 burger.onclick = () => {
-	burger.classList.toggle('_active');
+	document.body.classList.toggle('menu_active');
 };
 
+// * swiper
 let init = false;
 let swiper;
 function swiperCard() {
 	if (window.innerWidth <= 700) {
 		if (init) return;
 
-		document.querySelector('.fields__swiper').classList.add('.swiper');
-		document.querySelector('.fields__list').classList.add('.swiper-wrapper');
+		document.querySelector('.fields__swiper').classList.add('swiper');
+		document.querySelector('.fields__list').classList.add('swiper-wrapper');
 		document
 			.querySelectorAll('.fields__item')
-			.forEach((el) => el.classList.add('.swiper-slide'));
+			.forEach((el) => el.classList.add('swiper-slide'));
 		init = true;
 		swiper = new Swiper('.swiper', {
 			spaceBetween: 10,
 			allowTouchMove: true,
 			pagination: {
 				el: '.swiper-pagination',
-				bulletClass: 'swiper-bullet',
-				bulletActiveClass: '_active',
+				// bulletClass: 'swiper-bullet',
+				// bulletActiveClass: '_active',
 				clickable: true,
+				dynamicBullets: true,
+				dynamicMainBullets: 1,
 			},
 		});
 	} else if (init) {
 		swiper.destroy();
 		init = false;
-		document.querySelector('.fields__swiper').classList.remove('.swiper');
-		document.querySelector('.fields__list').classList.remove('.swiper-wrapper');
+		document.querySelector('.fields__swiper').classList.remove('swiper');
+		document.querySelector('.fields__list').classList.remove('swiper-wrapper');
 		document
 			.querySelectorAll('.fields__item')
-			.forEach((el) => el.classList.remove('.swiper-slide'));
+			.forEach((el) => el.classList.remove('swiper-slide'));
 	}
 }
-swiperCard();
+window.addEventListener('DOMContentLoaded', swiperCard);
 window.addEventListener('resize', swiperCard);
+
+// * textarea auto height
+const textarea_input = document.querySelector('.form-contact textarea');
+const textareaAutoHeight = () => {
+	textarea_input.style.height = 0;
+	textarea_input.style.height = textarea_input.scrollHeight + 'px';
+};
+
+textarea_input.addEventListener('input', textareaAutoHeight);
+window.addEventListener('DOMContentLoaded', textareaAutoHeight);
+window.addEventListener('resize', textareaAutoHeight);
